a8ea22deb2dc7ba5a48687e912753655c624db8a
susyimes25 <sujihua@mkit.com.cn>
2017-07-18 17:41:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/susyimes/cmakeuninstallfeedbackexample/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.susyimes.cmakeuninstallfeedbackexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.susyimes.cmakeuninstallfeedback.FeedbackUtils;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FeedbackUtils.openUrlWhenUninstall(this, "http://www.baidu.com");
+    }
+}
